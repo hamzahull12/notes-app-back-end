8c3fbad5ecfd9b950d42e27097cb3441758e36af
hamzahmaulana212@gmail.com <hamzahmaulana212@gmail.com>
2024-04-09 06:43:58 +0700

    add foreign key to notes owner column

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -15,7 +15,6 @@ class NotesHandler {
 
     const { title = 'untitled', body, tags } = request.payload;
     const { id: credentialId } = request.auth.credentials;
-    console.log(credentialId);
     const noteId = await this._service.addNote({
       title, body, tags, owner: credentialId,
     });
